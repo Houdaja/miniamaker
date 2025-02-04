9318ae9f7999708b99c495be054b60be3397043d
houdaja <houdajaadar@gmail.com>
2025-02-04 09:26:15 +0100

    Ajout de la gestion des liens de connexion par email et redirection des utilisateurs connectés vers leur profil lors de l'inscription.

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -26,7 +26,10 @@ class RegistrationController extends AbstractController
 
     #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager): Response
-    {
+    {   //Si l'utilisateur est déjà connecté, on le redirige vers son profil
+        if($this->getUser()) {
+            return $this->redirectToRoute('app_profile');
+        }
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
