6aee804711692a16667f3a672afb20e7994e33a5
houdaja <houdajaadar@gmail.com>
2025-02-04 16:58:16 +0100

    Ajout d'un constructeur à l'entité LoginHistory pour initialiser la date de connexion à la date actuelle.

--- src/Entity/LoginHistory.php
+++ src/Entity/LoginHistory.php
@@ -33,6 +33,11 @@ class LoginHistory
     #[ORM\Column(length: 255)]
     private ?string $browser = null;
 
+    public function __construct()
+    {
+        $this->login_date = new \DateTimeImmutable();
+    }
+    
     public function getId(): ?int
     {
         return $this->id;
