5e80a479b86d7995bfbdf6f2a3f66300f762f895
houdaja <houdajaadar@gmail.com>
2025-02-05 15:07:10 +0100

    Amélioration de la gestion de l'historique de connexion et vérification du profil utilisateur complet dans le contrôleur de page

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -13,24 +13,28 @@ final class PageController extends AbstractController
     #[Route('/', name: 'app_homepage', methods: ['GET'])]
     public function index(Request $request, LoginHistoryService $lhs): Response
     {
-        if (
-                $this->getUser() && // Si l'utilisateur est connecté
-                $request->headers->get('referer') == 'https://127.0.0.1:8000/login'
-            ) 
-        {
-            $lhs->addHistory(
-                $this->getUser(),
-                $request->headers->get('user-agent'),
-                $request->getClientIp()
-            );
-        }
-
-        //dd($request);
-
         if (!$this->getUser()) {
             return $this->render('page/lp.html.twig');
+        } else {
+            $requestArray = [
+                "fromLogin" => $this->getParameter('APP_URL') . $this->generateUrl('app_login'),
+                "referer" => $request->headers->get('referer'),
+                "ip" => $request->getClientIp(),
+                "userAgent" => $request->headers->get('user-agent'),
+            ];
+
+            // Lancement du LoginHistoryService s'il vient de la connexion
+            if ($requestArray['fromLogin'] === $requestArray['referer']) {
+                $lhs->addHistory($this->getUser(), $requestArray['userAgent'], $requestArray['ip']);
+            }
+
+            // Vérification du profil complet d'utilisateur
+            if (!$this->getUser()->isComplete()) {
+                return $this->render('page/complete.html.twig');
+            }
         }
+
         return $this->render('page/homepage.html.twig');
     }
+
 }
-                    
