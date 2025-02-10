8c262f91321acdda8ccda3d8989be9973dbd92ea
houdaja <houdajaadar@gmail.com>
2025-02-10 17:00:10 +0100

    Ajout de LiipImagineBundle dans la configuration des bundles

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
