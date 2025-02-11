25cbb2604023dbc16e849c12c67f858b55760008
houdaja <houdajaadar@gmail.com>
2025-02-11 15:34:59 +0100

    Ajout de la logique de traitement des abonnements dans le contrôleur d'abonnement avec vérification de l'abonnement actif et gestion des paiements.

--- src/Controller/SubscriptionController.php
+++ src/Controller/SubscriptionController.php
@@ -2,17 +2,37 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Service\PaymentService;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+#[IsGranted('ROLE_USER')]
 final class SubscriptionController extends AbstractController
 {
-    #[Route('/subscription', name: 'app_subscription')]
-    public function index(): Response
+
+    // public function __construct(private Request $request)
+    // {
+    //     $this->request = $request;
+    // }
+
+    #[Route('/subscription', name: 'app_subscription', methods: ['POST'])]
+    public function subscription(PaymentService $ps , Request $request): Response
     {
-        return $this->render('subscription/index.html.twig', [
-            'controller_name' => 'SubscriptionController',
-        ]);
+        $subscription = $this->getUser()->getSubscription();
+        if ($subscription == null || $subscription->isActive() === false) {
+            $ps->setPayment(
+                $this->getUser(),
+                intval($request->get('plan'))
+
+                // $this->request->getPayload('plan')
+            );
+        } else {
+            $this->addFlash('warning', 'Vous avez déjà un abonnement actif');
+            return $this->redirectToRoute('app_profile');
+        }
     }
 }
+                    
